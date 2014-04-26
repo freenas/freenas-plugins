ed0f3182f77e06405af138c94d22c491443fae08
Martin Grohmann <martin@medi-inf.org>
2014-04-26 19:43:27 +0200

    Added plexmediaserver plexpass version

---
+++ plugins/plexmediaserver-plexpass/resources/plexmediaserverUI/freenas/models.py
@@ -0,0 +1,15 @@
+import platform
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class PlexMediaServer(models.Model):
+    """
+    Django model describing every tunable setting for plexmediaserver
+    """
+
+    enable = models.BooleanField(default=False)
+    disable_remote_security = models.BooleanField(
+        default=False,
+        verbose_name=_("Disable remote security")
+    )
