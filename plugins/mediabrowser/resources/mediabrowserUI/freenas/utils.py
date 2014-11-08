48fcb2cd132b10fb1fb663710ebaf2cb1469c4ad
josh4trunks <joshruehlig@gmail.com>
2014-11-08 00:17:17 -0800

    Add plugin for "Media Browser"

---
+++ plugins/mediabrowser/resources/mediabrowserUI/freenas/utils.py
@@ -0,0 +1,41 @@
+from subprocess import Popen, PIPE
+import hashlib
+import os
+import platform
+
+mediabrowser_pbi_path = "/usr/pbi/mediabrowser-" + platform.machine()
+mediabrowser_etc_path = os.path.join(mediabrowser_pbi_path, "etc")
+mediabrowser_fcgi_pidfile = "/var/run/mediabrowser_fcgi_server.pid"
+mediabrowser_control = "/usr/local/etc/rc.d/mediabrowser"
+mediabrowser_icon = os.path.join(mediabrowser_pbi_path, "default.png")
+mediabrowser_oauth_file = os.path.join(mediabrowser_pbi_path, ".oauth")
+
+
+def get_rpc_url(request):
+    return 'http%s://%s:%s/plugins/json-rpc/v1/' % (
+        's' if request.is_secure() else '',
+        request.META.get("SERVER_ADDR"),
+        request.META.get("SERVER_PORT"),
+        )
+
+
+def get_mediabrowser_oauth_creds():
+    f = open(mediabrowser_oauth_file)
+    lines = f.readlines()
+    f.close()
+
+    key = secret = None
+    for l in lines:
+        l = l.strip()
+
+        if l.startswith("key"):
+            pair = l.split("=")
+            if len(pair) > 1:
+                key = pair[1].strip()
+
+        elif l.startswith("secret"):
+            pair = l.split("=")
+            if len(pair) > 1:
+                secret = pair[1].strip()
+
+    return key, secret
